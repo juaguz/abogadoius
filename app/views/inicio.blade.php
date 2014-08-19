88c86999fdd229e79d9d71cf107c72ca481069a1
juaguz <juanguzman91@gmail.com>
2014-08-19 09:51:04 -0300

    first commit

---
+++ app/views/inicio.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<!-- saved from url=(0042)http://wrapbootstrap.com/preview/WB0329559 -->
+<html lang="en-us">
+    <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <title>AbogadoIus</title>
+    <meta name="description" content="{{ $contenido or "" }}">
+    <meta name="viewport" content="width=1024">
+    <!--[if lt IE 9]>
+    <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+    <!--<link rel="stylesheet" type="text/css" media="screen" href="./ONELAW - Onepage Template for Lawyers - Live Preview - WrapBootstrap_files/screen_preview.css">-->
+    {{ HTML::style('assets/css/screen_preview.css') }}
+    <!--<script src="//cdn.optimizely.com/js/233905874.js"></script>-->
+    {{ HTML::script('assets/js/behavior.js') }}
+    <script type="text/javascript">
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-28871117-1']);
+        _gaq.push(['_setDomainName', 'wrapbootstrap.com']);
+        _gaq.push(['_trackPageview']);
+        (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+    </script>
+    <link rel="shortcut icon" href="http://d85wutc1n854v.cloudfront.net/live/imgs/favicon.ico">
+</head>
+<body>
+
+<script>
+    $(document).ready(function(){
+        function fix_height(){
+            var h = $("#tray").height();
+            $("#preview").attr("height", (($(window).height()) - h) + "px");
+        }
+        $(window).resize(function(){ fix_height(); }).resize();
+        //$("#preview").contentWindow.focus();
+    });
+</script>
+
+
+<script type="text/javascript" src="chrome-extension://bmdblncegkenkacieihfhpjfppoconhi/in-page-script.js"></script></body></html>
